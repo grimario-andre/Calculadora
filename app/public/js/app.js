98a7fb9694c685d240a13776b766216e718ad405
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2023-06-03 14:58:16 -0300

    Create page calculator

---
+++ app/public/js/app.js
@@ -0,0 +1,4 @@
+// Obtém o ano atual
+const currentYear = new Date().getFullYear();
+// Insere o ano atual no elemento span com o id "currentYear"
+document.getElementById('currentYear').textContent = currentYear;
